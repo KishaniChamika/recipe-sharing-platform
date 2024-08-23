c915fb88482c374ab8cccadfaeb51fe03e75e921
KishaniChamika <adkishani@gmail.com>
2024-08-24 01:07:08 +0530

    Set up React client with TypeScript
    - Created React app with TypeScript template
    - Removed unwanted default files
    - Customized App component and styling for a simple UI
    - Set up basic folder structure for components and pages
    - Installed React Router and Axios with TypeScript support
    -Add initial folders with placeholders

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to My Simple Client Interface</h1>
+      {/* Add your routes or components here */}
+    </div>
+  );
+};
+
+export default App;
