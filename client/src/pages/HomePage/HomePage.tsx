0ff2e642363726f8a18701a8fd9d0047d3fcebeb
KishaniChamika <adkishani@gmail.com>
2024-08-24 12:52:28 +0530

    Add banner section to HomePage component

--- client/src/pages/HomePage/HomePage.tsx
+++ client/src/pages/HomePage/HomePage.tsx
@@ -7,3 +7,17 @@ const searchIcon = require('./search-icon.png');
 const userIcon = require('./user-icon.png');
 const favoritesIcon = require('./favorites-icon.png');
 const categoryIcon = require('./category-icon.png');
+
+const HomePage: React.FC = () => {
+    return (
+      <div className="home-container">
+        <div className="banner">
+          <div className="banner-content">
+            <h1>Welcome to RecipeHub</h1>
+            <p>Where Every Recipe Tells a Story - Discover, Create, Share!</p>
+            <a href="https://www.recipehub.com/login" className="get-started-button">Get Started</a>
+          </div>
+        </div>
+      </div>
+    );
+};  
