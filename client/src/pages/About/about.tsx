06c603dc5ace6bb24d90443f6a0529093e187f21
Hansama Dewindi Rajaguru <141362143+HansamaRajaguru@users.noreply.github.com>
2024-08-24 20:14:02 +0530

    Add content of about.tsx

--- client/src/pages/About/about.tsx
+++ client/src/pages/About/about.tsx
@@ -2,6 +2,27 @@ import React from "react";
 
 export const About:React.FC=()=>{
     return(
-        <div>About</div>
+        <div>
+            <div>
+                <h1>About Us</h1>
+            </div>
+            <div>
+            <h3>Welcome to Recipe Hub!</h3>
+                
+                <p>Where flavors meet creativity and culinary passion knows no bounds. Our platform is a melting pot of diverse tastes, offering a space for food lovers to discover, create, and share recipes that inspire and delight. Whether you're a seasoned chef or a home cook, join us in celebrating the art of cooking.</p>
+            
+                    <p>We are a group of enthusiastic undergraduates from the Department of ICT at the University of Sri Jayewardenepura. This website is the culmination of our 3rd-year final project for the Web Application Development course module. Our mission is to unite food enthusiasts across the globe, fostering a vibrant community that celebrates culinary creativity. Meet our team....</p>    
+                    <ul>
+                        <li> A.D.K.Chamika</li>
+                        <li>P.G.B.Malshani</li>
+                        <li>R.M.H.D.Rajaguru</li> 
+                        <li> D.D.T.O.Wickramaratne</li>
+                    </ul>
+                    
+            </div>
+            
+        </div>
+        
+        
     );
 }
