fb88bb8dd1eb0c2048d1ad894aacdf510f31943b
KishaniChamika <adkishani@gmail.com>
2024-08-24 12:48:54 +0530

    Import images for HomePage icons

--- client/src/pages/HomePage/HomePage.tsx
+++ client/src/pages/HomePage/HomePage.tsx
@@ -1,2 +1,9 @@
 import React from 'react';
 import './HomePage.css';
+
+// Importing images using require()
+const homeIcon = require('./home-icon.png');
+const searchIcon = require('./search-icon.png');
+const userIcon = require('./user-icon.png');
+const favoritesIcon = require('./favorites-icon.png');
+const categoryIcon = require('./category-icon.png');
