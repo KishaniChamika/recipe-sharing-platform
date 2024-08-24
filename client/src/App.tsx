dfa143c6871cd31ff6c28ba288b2896db6fc1565
KishaniChamika <adkishani@gmail.com>
2024-08-24 14:19:10 +0530

    Update App.tsx to include React Router and HomePage component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
+// Import other components/pages as needed
 
 const App: React.FC = () => {
   return (
-    <div>
-      <h1>Welcome to My Simple Client Interface</h1>
-      {/* Add your routes or components here */}
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {/* Define other routes here */}
+      </Routes>
+    </Router>
   );
 };
 
