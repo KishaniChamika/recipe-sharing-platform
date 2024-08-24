dac3ecc5f3171fc8eb8d0ed1bc34a11cce98c3fb
Hansama Dewindi Rajaguru <141362143+HansamaRajaguru@users.noreply.github.com>
2024-08-24 14:33:20 +0530

    create structure of register page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Login } from './pages/Authentication/login';
+import { Register } from './pages/Authentication/register';
 const App: React.FC = () => {
   return (
     <div>
       
-      <Login/>
+      <Register/>
     </div>
   );
 };
--- client/src/pages/Authentication/register.tsx
+++ client/src/pages/Authentication/register.tsx
@@ -1,7 +1,49 @@
-import React from "react";
+import React,{useState} from "react";
 
 export const Register=()=>{
+    const [username, setUsername] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
+
     return(
-        <div></div>
+        <div className='auth-background'>
+        <div className="wrapper">
+        <form >
+                <h1>REGISTER</h1>
+
+                <div className="input-box">
+                    <label htmlFor="username">Username: </label>
+                    <input
+                        type="text"
+                        id="username"
+                        value={username}
+                        placeholder="Enter your username"
+                    />
+                </div>
+                <div className="input-box">
+                    <label htmlFor="email">Email: </label>
+                    <input
+                        type="email"
+                        id="email"
+                        value={email}
+                        placeholder="Enter your email address"
+                    />
+                </div>
+                <div className="input-box">
+                    <label htmlFor="password">Password: </label>
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        placeholder="Enter your password"
+                    />
+                </div>
+
+                
+                
+            </form>
+            </div>
+        </div>
+        
     )
 }
