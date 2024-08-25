26c1d4add9019a0046f56b8c680e949172067c77
BhagyaMalshani <pbhagyamalshani2000@gmail.com>
2024-08-25 21:53:21 +0530

    Add AddRecipe component and its styling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,25 @@
+// src/App.tsx
+
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
-// Import other components/pages as needed
 import { Auth } from './pages/Authentication/auth';
+import RecipeView from './pages/Recipes/RecipeView/RecipeView';
+import AddRecipe from './pages/Recipes/AddRecipe/AddRecipe';
+import RecipeDetails from './pages/Recipes/RecipeDetails/RecipeDetails';
+
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-      <Route path="/" element={<HomePage />} />
+        {/* Your friend's routes */}
+        <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Auth />} />
+
+        {/* Your routes */}
+        <Route path="/recipe-view" element={<RecipeView />} />
+        <Route path="/add-recipe" element={<AddRecipe />} />
+        <Route path="/recipe/:id" element={<RecipeDetails />} />
       </Routes>
     </Router>
   );
