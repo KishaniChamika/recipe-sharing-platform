4f6bc831525a289cad69b2bd89b973599fe42f92
Hansama Dewindi Rajaguru <141362143+HansamaRajaguru@users.noreply.github.com>
2024-08-25 22:21:27 +0530

    Add profile display and edit functionality in ProfilePage component

--- client/src/pages/UserProfile/profile.tsx
+++ client/src/pages/UserProfile/profile.tsx
@@ -22,21 +22,40 @@ export const ProfilePage:React.FC=()=>{
         <div className="container">
             <div className="profile-wrapper">
                 <h2>User Profile</h2>
-                <div className="info">
-                    <p><strong>Username:</strong></p>
-                    <p><strong>Email:</strong></p>
-                    <p><label>First Name:</label></p>
-                    <p><label>Last Name:</label></p>
-                    <p><label>Bio:</label></p>
-                    <p><label>Avatar:</label></p>
-                    <div className="button-group">
-                        <button>Edit Profile</button>
-                        <button>Logout</button>
+                
+                    <div>
+                        <div className="avatar">
+                            
+                                <img  alt="Avatar" />
+                            
+                            
+                        </div>
+                        <div className="info">
+                            <p><strong>Username:&nbsp;&nbsp;&nbsp;</strong> </p>
+                            <p><strong>Email:&nbsp;&nbsp;&nbsp;</strong> </p>
+                            {editMode ? (
+                                <>
+                                    <label>First Name:&nbsp;&nbsp; &nbsp;<input type="text" value={firstname} onChange={(e) => setFirstname(e.target.value)} /></label>
+                                    <label>Last Name:&nbsp;&nbsp;&nbsp; <input type="text" value={lastname} onChange={(e) => setLastname(e.target.value)} /></label>
+                                    <label>Bio:&nbsp;&nbsp; &nbsp; <textarea value={bio} onChange={(e) => setBio(e.target.value)} /></label>
+                                    <label>Avatar:&nbsp;&nbsp; &nbsp; <input type="file" accept="image/*" /></label>
+                                    <button >Save</button>
+                                </>
+                            ) : (
+                                <>
+                                    <p><strong>First Name:&nbsp;&nbsp;&nbsp;</strong> </p>
+                                    <p><strong>Last Name:&nbsp;&nbsp;&nbsp;</strong> </p>
+                                    <p><strong>Bio:&nbsp;&nbsp;&nbsp;</strong> </p>
+                                    <div className="button-group">
+                                        <button onClick={() => setEditMode(true)}>Edit Profile</button>
+                                        <button >Logout</button>
+                                    </div>
+                                </>
+                            )}
+                        </div>
                     </div>
-                </div>
                 
             </div>
-            
         </div>
     )
 }
