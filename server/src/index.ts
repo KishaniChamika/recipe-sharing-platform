74719d7befe92a1587fede0aa496c1f69caed025
Hansama Dewindi Rajaguru <141362143+HansamaRajaguru@users.noreply.github.com>
2024-08-25 08:50:45 +0530

    Added Express server setup with CORS, body-parser, and MongoDB connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,26 @@
-console.log('Hello world!')
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import cors from 'cors';
+
+const app=express();
+
+app.use(cors({
+    origin: 'http://localhost:3000'
+}))
+
+app.use(bodyParser.json());
+
+mongoose.connect(
+    "mongodb+srv://adkishani:dbRecipeApp@cluster0.c8wjr.mongodb.net/RecipeApp?retryWrites=true&w=majority", {
+    dbName: "RecipeApp",
+    }
+)
+.then(() => console.log("MongoDB is Connected"))
+.catch((err) => console.log(err.message));
+
+
+
+const port=3006;
+
+app.listen(port,()=>console.log(`server is running on port ${port}`))
