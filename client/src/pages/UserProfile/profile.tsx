b8b404e1c492e8b7742b323f9219931d5702ee7b
Hansama Dewindi Rajaguru <141362143+HansamaRajaguru@users.noreply.github.com>
2024-08-25 20:50:12 +0530

    Setup UserProfile interface

--- client/src/pages/UserProfile/profile.tsx
+++ client/src/pages/UserProfile/profile.tsx
@@ -1,5 +1,14 @@
 import React from "react";
 
+interface UserProfile {
+    username: string;
+    email: string;
+    firstname?: string;
+    lastname?: string;
+    bio?: string;
+    avatar?: string;
+}
+
 export const ProfilePage:React.FC=()=>{
     return(
         <div>Profile</div>
