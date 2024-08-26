dd8fa19ac80bc5bfd3769bc0553128d305b4e78f
KishaniChamika <adkishani@gmail.com>
2024-08-27 02:46:27 +0530

    Delete unwanted files

--- client/src/pages/RecipeCategoriesPage/RecipeCategoryPage.tsx
+++ client/src/pages/RecipeCategoriesPage/RecipeCategoryPage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import './CategoryRecipe.css'; // Assuming this is where you'd style the recipe page
+
 
 const RecipeCategoryPage: React.FC = () => {
   const { category } = useParams<{ category: string }>();
