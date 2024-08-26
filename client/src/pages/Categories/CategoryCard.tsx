da9f6ed6a28c74b32199a51b1e3440fbc977972d
KishaniChamika <adkishani@gmail.com>
2024-08-27 02:12:29 +0530

    Define CategoryCard component structure with props and JSX layout

--- client/src/pages/Categories/CategoryCard.tsx
+++ client/src/pages/Categories/CategoryCard.tsx
@@ -6,3 +6,13 @@ interface CategoryCardProps {
   image: string;
   onClick: () => void;
 }
+
+const CategoryCard: React.FC<CategoryCardProps> = ({ name, image, onClick }) => {
+    return (
+      <div className="category-card" onClick={onClick}>
+        <img src={image} alt={name} className="category-image" />
+        <div className="category-title">{name}</div>
+      </div>
+    );
+};
+  
