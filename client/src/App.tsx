207e7e5f814ce3872f05fd16404f1a374b21cfbc
KishaniChamika <adkishani@gmail.com>
2024-08-27 04:16:41 +0530

    Integrate routing and Navbar into App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,18 +9,30 @@ import AddRecipe from './pages/Recipes/AddRecipe/AddRecipe';
 import RecipeDetails from './pages/Recipes/RecipeDetails/RecipeDetails';
 import Categories from './pages/Categories/Categories';
 import RecipeCategoryPage from './pages/RecipeCategoriesPage/RecipeCategoryPage';
+//import Favorite from './pages/Favorites';
+import UserProfile from './pages/UserProfile';
+import { About } from './pages/About/about'; // Named import
+import Navbar from './components/Navbar';
 
 const App: React.FC = () => {
+  const isLoggedIn = false; // Set this based on authentication status
+
+  //add this later <Route path="/favorites" element={<Favorite />} />
+
   return (
     <Router>
+      <Navbar isLoggedIn={isLoggedIn} />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Auth />} />
-        <Route path="/" element={<Categories />} />
+        <Route path="/categories" element={<Categories />} />
         <Route path="/recipes/:category" element={<RecipeCategoryPage />} />
         <Route path="/recipe-view" element={<RecipeView />} />
         <Route path="/add-recipe" element={<AddRecipe />} />
         <Route path="/recipe/:id" element={<RecipeDetails />} />
+        
+        <Route path="/profile" element={<UserProfile />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </Router>
   );
