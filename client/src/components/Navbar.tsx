c8b3d7df62d5a167a99583745e937273d7b7f59b
KishaniChamika <adkishani@gmail.com>
2024-08-27 03:39:03 +0530

    Create Navbar component structure and add navigation links

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -6,3 +6,19 @@ interface NavbarProps {
   isLoggedIn: boolean;
 }
 
+const Navbar: React.FC<NavbarProps> = ({ isLoggedIn }) => {
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <Link to="/">RecipeHub</Link>
+      </div>
+      <ul className="nav-links">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/recipes">Recipes</Link></li>
+        <li><Link to="/categories">Categories</Link></li>
+        <li><Link to="/about">About Us</Link></li>
+        <li><Link to="/search">Search</Link></li> {/* Changed search bar to a link */}
+      </ul>
+    </nav>
+  );
+};
