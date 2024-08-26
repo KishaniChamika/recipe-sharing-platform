18124a02b2f37870fa5f66d8e580b3cdefa4af66
KishaniChamika <adkishani@gmail.com>
2024-08-27 02:41:52 +0530

    Add necessary imports for routing and component integration in App.tsx and Set up React Router with routes for Categories and RecipeCategoryPage components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,16 +7,17 @@ import { Auth } from './pages/Authentication/auth';
 import RecipeView from './pages/Recipes/RecipeView/RecipeView';
 import AddRecipe from './pages/Recipes/AddRecipe/AddRecipe';
 import RecipeDetails from './pages/Recipes/RecipeDetails/RecipeDetails';
+import Categories from './pages/Categories/Categories';
+import RecipeCategoryPage from './pages/RecipeCategoriesPage/RecipeCategoryPage';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        {/* Your friend's routes */}
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Auth />} />
-
-        {/* Your routes */}
+        <Route path="/" element={<Categories />} />
+        <Route path="/recipes/:category" element={<RecipeCategoryPage />} />
         <Route path="/recipe-view" element={<RecipeView />} />
         <Route path="/add-recipe" element={<AddRecipe />} />
         <Route path="/recipe/:id" element={<RecipeDetails />} />
