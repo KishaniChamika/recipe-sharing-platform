534ca70f9c3d0e288a12bc12bb0b0c51358eca0b
KishaniChamika <adkishani@gmail.com>
2024-08-28 12:11:07 +0530

    Ensure integration with updated Login and Register logic

--- client/src/pages/Authentication/auth.tsx
+++ client/src/pages/Authentication/auth.tsx
@@ -1,24 +1,27 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import { Login } from "./login";
 import { Register } from "./register";
 
-export const Auth:React.FC=()=>{
-    const [showLogin, setShowLogin] = useState(true);
-
-    const switchForm = () => {
-        setShowLogin(!showLogin);
-    };
-
-    return (
-        <div className="auth">
-            <div >
-                {showLogin ? (
-                    <Login switchForm={switchForm}/>
-                ) : (
-                    <Register switchForm={switchForm}/>
-                )}
-            </div>
-        </div>
-    );
-
+interface AuthProps {
+  onLogin: (status: boolean) => void;
 }
+
+export const Auth: React.FC<AuthProps> = ({ onLogin }) => {
+  const [showLogin, setShowLogin] = useState(true);
+
+  const switchForm = () => {
+    setShowLogin(!showLogin);
+  };
+
+  return (
+    <div className="auth">
+      <div>
+        {showLogin ? (
+          <Login switchForm={switchForm} onLogin={onLogin} />
+        ) : (
+          <Register switchForm={switchForm} />
+        )}
+      </div>
+    </div>
+  );
+};
